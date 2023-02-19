b1ffda118fb1feb139d091b10b64515f8a7785f8
Atakan Tümoğlu <atakantumoglu@msn.com>
2023-02-20 01:19:18 +0300

    test commit

--- InventoryService.Api/Controllers/PersonelController.cs
+++ InventoryService.Api/Controllers/PersonelController.cs
@@ -22,7 +22,15 @@ namespace InventoryService.Api.Controllers
             _mapper = mapper;
         }
 
-
+        [HttpPut]
+        public async Task<ActionResult> Update(PersonelUpdateDto personelDto)
+        {
+            var id = from p in _context.Personels where p.Id == personelDto.Id select p;
+            var personelUpdate = _mapper.Map<Personal>(personelDto);
+          
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
 
         [HttpPost]
         public async Task<ActionResult> Create(PersonelCreateDto personelDto)
