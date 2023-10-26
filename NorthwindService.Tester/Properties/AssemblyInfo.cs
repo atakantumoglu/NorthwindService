04d3aa84d50ceb3a1f52338ab6c8ac388b8e6763
Atakan Tümoğlu <atakantumoglu@msn.com>
2023-10-26 20:10:35 +0300

    asd

--- NorthwindService.Tester/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("9a94f530-3eec-46ad-a861-8d29cb45ed4c")]
