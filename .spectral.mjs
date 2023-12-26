c218622e86012f0b5d4e428a93a1529dc60ca35f
Atakan Tümoğlu <atakantumoglu@me.com>
2023-12-26 08:35:47 +0300

    Added 5 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMDUzMw/spectral.js?branch_slug=dev&token=e3f0d8fd-3b49-4492-b1d9-202dbcafa531";
+export default { extends: ruleset };
